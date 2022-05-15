aebda5719b0d5298aed000f5b5d81338fbeb8273
Eray-Adiyaman <erayadiyaman@outlook.com>
2022-05-15 15:22:31 +0300

    flipping

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import './App.css';
 import Card from './components/Card';
 
 const cardImages = [
-  {"src": "/img/helmet-1.png"},
-  {"src": "/img/potion-1.png"},
-  {"src": "/img/ring-1.png"},
-  {"src": "/img/scroll-1.png"},
-  {"src": "/img/shield-1.png"},
-  {"src": "/img/sword-1.png"},
+  {"src": "/img/helmet-1.png", matched: false},
+  {"src": "/img/potion-1.png", matched: false},
+  {"src": "/img/ring-1.png", matched: false},
+  {"src": "/img/scroll-1.png", matched: false},
+  {"src": "/img/shield-1.png", matched: false},
+  {"src": "/img/sword-1.png", matched: false},
 ]
 
 function App() {
@@ -38,19 +38,32 @@ function App() {
     setTurns(prev => prev + 1)
   }
 
+
   useEffect(()=>{
     if(firstChoise && secondChoise){
       if(firstChoise.src === secondChoise.src){
         console.log("match!")
+        setCards(prevCards => {
+          return prevCards.map(card => {
+            if(card.src === firstChoise.src){
+              return {...card, matched:true}
+            }else{
+              return card
+            }
+          })
+        })
         resetTurn()
       }else{
-        console.log("no match!")
-        resetTurn()
+        setTimeout(() => {
+          resetTurn()
+        }, 1000);
+        
       }
     }
 
   },[firstChoise,secondChoise])
 
+  console.log(cards)
 
   return (
     <div className="App">
@@ -63,6 +76,7 @@ function App() {
               key={card.id} 
               card={card}
               handleChoice={handleChoice}
+              flipped={card === firstChoise || card === secondChoise || card.matched }
               />
           ))
         }
